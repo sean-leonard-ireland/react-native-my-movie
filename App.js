20f8ab444623e551dd388d61ec6263aee0d61bcd
Sean Leonard <seanleonardsap@gmail.com>
2021-03-21 14:05:27 +0000

    NavigationContainer + Stack.Navigator

--- App.js
+++ App.js
@@ -1,15 +1,22 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 
+import HomeScreen from "./src/screens/HomeScreen";
+import DetailScreen from "./src/screens/DetailsScreen";
+import ImageScreen from "./src/screens/ImageScreen";
+
+const Stack = createStackNavigator();
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
