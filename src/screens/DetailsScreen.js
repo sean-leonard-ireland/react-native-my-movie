a7427347586b9bb5d4b5df828325ced908f8a46a
Sean Leonard <seanleonardsap@gmail.com>
2021-03-23 18:02:34 +0000

    simple screen paarameters

--- src/screens/DetailsScreen.js
+++ src/screens/DetailsScreen.js
@@ -2,22 +2,26 @@ import React from "react";
 import { Button, View, Text, StyleSheet } from "react-native";
 
 const DetailsScreen = ({ navigation, route }) => {
-
   console.log(route);
+  const { screenNumber } = route.params;
   return (
     <View style={styles.mainView}>
       <Text>Details Screen</Text>
+      <Text style={{ fontSize: 100 }}>{screenNumber}</Text>
       <Button
         title="Go to Image"
         onPress={() => {
           navigation.navigate("Image");
         }}
       />
-         <Button
+      <Button
         title="More Details"
         onPress={() => {
-          navigation.push("Details_to_Details");
-        }}/>
+          navigation.push("Details_to_Details", {
+            screenNumber: screenNumber + 1,
+          });
+        }}
+      />
       <Button
         title="Go Home"
         onPress={() => {
