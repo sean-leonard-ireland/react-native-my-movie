9f6f63d66db9aba9ba196f70e22b41c2099c68e1
Sean Leonard <seanleonardsap@gmail.com>
2021-03-26 17:55:08 +0000

    Template strings

--- src/screens/DetailsScreen.js
+++ src/screens/DetailsScreen.js
@@ -2,23 +2,23 @@ import React, { useEffect, useState } from "react";
 import { Button, View, Text, StyleSheet } from "react-native";
 
 const DetailsScreen = ({ navigation, route }) => {
-  //console.log(route);
   const movie = route.params.movie;
+  //console.log(movie.title);
+  //console.log(movie.release);
   const [movieDetails, setMovieDetails] = useState(null);
 
   useEffect(() => {
     const xhr = new XMLHttpRequest();
     xhr.open(
       "GET",
-      "http://www.omdbapi.com/?apikey=ce09e2b7&t=Star+Wars&y=1977"
+      `http://www.omdbapi.com/?apikey=ce09e2b7&t=${movie.title}&y=${movie.release}`
     );
     xhr.send();
     xhr.onload = () => {
       if (xhr.status == 200) {
         let response = JSON.parse(xhr.response);
-        //console.log(response.Title);
-        //console.log(response.Released);
-        //console.log(response.Plot);
+        console.log(response);
+        //console.log(response.Writer);
         setMovieDetails(response);
       } else {
         console.log(`http bad respsonse ${xhr.status}`);
