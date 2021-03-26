1706075562e4f2a31df5d26a1afd2c79966d7cb6
Sean Leonard <seanleonardsap@gmail.com>
2021-03-26 17:10:05 +0000

    XMLHttpRequest + useEffect + onLoad()

--- src/screens/DetailsScreen.js
+++ src/screens/DetailsScreen.js
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Button, View, Text, StyleSheet } from "react-native";
 
 const DetailsScreen = ({ navigation, route }) => {
-  console.log(route);
+  //console.log(route);
   const movie = route.params.movie;
+
+  useEffect(() => {
+    const xhr = new XMLHttpRequest();
+    xhr.open(
+      "GET",
+      "http://www.omdbapi.com/?apikey=ce09e2b7&t=Star+Wars&y=1977"
+    );
+    xhr.send();
+    xhr.onload = () => {
+      console.log(xhr.response);
+    };
+  }, []);
 
   return (
     <View style={styles.mainView}>
